1cd676615fcffce2ad7834968c6f78afc73824d5
yusam <yusam@yandex.ru>
2023-03-09 10:07:38 +0400

    #changed

--- src/functions.php
+++ src/functions.php
@@ -1,12 +1,11 @@
 <?php
 
+if (!defined('JSON_EXT_CONTENT_TYPE')) {
+    define('JSON_EXT_CONTENT_TYPE', 'application/json');
+}
+
 if (! function_exists('json_ext_json_encode_unescaped')) {
 
-    /**
-     * @param array $value
-     * @param int $flags
-     * @return string
-     */
     function json_ext_json_encode_unescaped(array $value, int $flags = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES): string
     {
         $result = strval(json_encode($value, $flags));
@@ -14,5 +13,39 @@ if (! function_exists('json_ext_json_encode_unescaped')) {
             $result = '{}';
         }
         return $result;
+    }
+}
+
+if (! function_exists('json_ext_ok')) {
+
+    function json_ext_ok(array $data = []): array
+    {
+        return [
+            'status' => 'ok',
+            'data' => $data,
+        ];
+    }
+}
+
+if (! function_exists('json_ext_error')) {
+
+    function json_ext_error(string $errorMessage, array $errorData = []): array
+    {
+        return [
+            'status' => 'error',
+            'errorMessage' => $errorMessage,
+            'errorData' => $errorData,
+        ];
+    }
+}
+
+if (! function_exists('json_ext_throwable')) {
+
+    function json_ext_throwable(\Throwable $e, array $errorData = []): array
+    {
+        if (method_exists($e, 'getData')) {
+            $errorData = array_merge($e->getData(), $errorData);
+        }
+        return json_ext_error($e->getMessage(), $errorData);
     }
 }
